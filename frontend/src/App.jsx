dab58225cbddd44c7ad18671adf819e19b00657c
Dlcuy22 <alifqwik@gmail.com>
2026-01-26 23:34:07 +0700

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+import logo from './assets/images/logo-universal.png';
+import './App.css';
+import {Greet} from "../wailsjs/go/main/App";
+
+function App() {
+    return (
+        <div id="App">
+ <h1>hi there</h1>
+        </div>
+    )
+}
+
+export default App
